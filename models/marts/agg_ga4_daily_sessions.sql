c6f540e4fabdd2b127d4cb157bcc51f5eae4f805
guido van kleef <guidovankleef@gmail.com>
2023-12-13 19:13:26 +0000

    added daily aggregations to model

---
+++ models/marts/agg_ga4_daily_sessions.sql
@@ -0,0 +1,38 @@
+{{
+    config(
+        materialized="incremental",
+        incremental_strategy="insert_overwrite",
+        partition_by={
+            "field": "session_start_date",
+            "data_type": "date",
+        }
+    )
+}}
+
+SELECT
+base.session_partition_date as session_start_date,
+session_number,
+session_default_channel_grouping,
+session_medium,
+session_source,
+session_campaign,
+session_content,
+landing_page_path,
+device_category,
+device_operating_system,
+device_web_info_browser,
+device_language,
+most_recent_traffic_type,
+geo_country,
+geo_region,
+count(distinct session_key) as sessions,
+sum(session_partition_count_page_views) as pageviews,
+count(distinct case when view_item_count > 0 then session_key end) as view_item_sessions,
+count(distinct case when add_to_cart_count > 0 then session_key end) as add_to_cart_sessions,
+count(distinct case when begin_checkout_count > 0 then session_key end) as begin_checkout_sessions,
+count(distinct case when purchase_count > 0 then session_key end) as purchases
+FROM
+  {{ ref('fct_ga4__sessions_daily') }} base
+left join {{ ref('dim_ga4__sessions_daily') }}
+using(session_key)
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14
